993b81c1fa91f409d6afd352cc2ad04f7e9a3c20
Chirag Baid <126846374+ChiragBaid@users.noreply.github.com>
2025-09-10 22:28:08 +0530

    Update middleware.go

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,48 +1,47 @@
 package middleware
 
 import (
-    "context"
-    "net/http"
-    "strings"
+	"context"
+	"net/http"
+	"strings"
 
-    "github.com/youruser/rate-limiter/internal/limiter"
+	"github.com/ChiragBaid/rate-limiter/internal/limiter"
 )
 
 type Config struct {
-    TokenBucket *limiter.TokenBucket
-    KeyFunc     func(r *http.Request) string // how to extract key (api-key header, ip, user)
-    TokensPerRequest int64
+	TokenBucket      *limiter.TokenBucket
+	KeyFunc          func(r *http.Request) string
+	TokensPerRequest int64
 }
 
 func NewRateLimitMiddleware(cfg Config) func(http.Handler) http.Handler {
-    return func(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            key := cfg.KeyFunc(r)
-            allowed, err := cfg.TokenBucket.Allow(context.Background(), key, cfg.TokensPerRequest)
-            if err != nil {
-                http.Error(w, "internal error", http.StatusInternalServerError)
-                return
-            }
-            if !allowed {
-                w.Header().Set("Retry-After", "1")
-                http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
-                return
-            }
-            next.ServeHTTP(w, r)
-        })
-    }
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			key := cfg.KeyFunc(r)
+			allowed, err := cfg.TokenBucket.Allow(context.Background(), key, cfg.TokensPerRequest)
+			if err != nil {
+				http.Error(w, "internal error", http.StatusInternalServerError)
+				return
+			}
+			if !allowed {
+				w.Header().Set("Retry-After", "1")
+				http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 func KeyByIP(r *http.Request) string {
-    ip := strings.Split(r.RemoteAddr, ":")[0]
-    return "ip:" + ip
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+	return "ip:" + ip
 }
 
 func KeyByHeader(r *http.Request) string {
-    v := r.Header.Get("X-API-Key")
-    if v == "" {
-        return KeyByIP(r)
-    }
-    return "api:" + v
+	v := r.Header.Get("X-API-Key")
+	if v == "" {
+		return KeyByIP(r)
+	}
+	return "api:" + v
 }
-
