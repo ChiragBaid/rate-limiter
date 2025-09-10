fcf981e7fc8a77f6a15e4e53e0a033fd9b6d4223
Chirag Baid <126846374+ChiragBaid@users.noreply.github.com>
2025-09-10 22:28:34 +0530

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,55 +1,50 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/youruser/rate-limiter/internal/backend"
-    "github.com/youruser/rate-limiter/internal/limiter"
-    "github.com/youruser/rate-limiter/internal/middleware"
+	"github.com/ChiragBaid/rate-limiter/internal/backend"
+	"github.com/ChiragBaid/rate-limiter/internal/limiter"
+	"github.com/ChiragBaid/rate-limiter/internal/middleware"
 )
 
 func main() {
-    // Read config from env
-    redisAddr := os.Getenv("REDIS_ADDR") // optional
-    var store limiter.Store
-    if redisAddr != "" {
-        rs := backend.NewRedisStore(redisAddr)
-        store = rs
-        fmt.Println("Using Redis store:", redisAddr)
-    } else {
-        ms := backend.NewMemStore()
-        store = ms
-        fmt.Println("Using In-Memory store")
-    }
+	redisAddr := os.Getenv("REDIS_ADDR")
+	var store limiter.Store
+	if redisAddr != "" {
+		rs := backend.NewRedisStore(redisAddr)
+		store = rs
+		fmt.Println("Using Redis store:", redisAddr)
+	} else {
+		ms := backend.NewMemStore()
+		store = ms
+		fmt.Println("Using In-Memory store")
+	}
 
-    // Example: 10 tokens/sec, burst capacity 20
-    tb := limiter.NewTokenBucket(10.0, 20, store)
+	tb := limiter.NewTokenBucket(10.0, 20, store)
 
-    mw := middleware.NewRateLimitMiddleware(middleware.Config{
-        TokenBucket: tb,
-        KeyFunc:     middleware.KeyByHeader,
-        TokensPerRequest: 1,
-    })
+	mw := middleware.NewRateLimitMiddleware(middleware.Config{
+		TokenBucket:      tb,
+		KeyFunc:          middleware.KeyByHeader,
+		TokensPerRequest: 1,
+	})
 
-    mux := http.NewServeMux()
-    mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "hello world")
-    })
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello world")
+	})
 
-    handler := mw(mux)
-    srv := &http.Server{
-        Addr:    ":8080",
-        Handler: handler,
-    }
+	handler := mw(mux)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
 
-    log.Println("listening on :8080")
-    if err := srv.ListenAndServe(); err != nil {
-        log.Fatal(err)
-    }
+	log.Println("listening on :8080")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
-
